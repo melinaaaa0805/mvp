3fbc882cc8a48b8b81efc9727c2f4c44bfd55368
melinaaaa0805 <melina.mitterrand08@gmail.com>
2026-01-18 16:54:04 +0100

    modif start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,15 @@
-npx prisma migrate dev
+#!/bin/sh
+# start.sh
+
+# Générer le client Prisma (optionnel si déjà généré dans builder)
+# npx prisma generate --schema=./prisma/schema.prisma
+
+# Appliquer les migrations sur la DB
 npx prisma migrate deploy
+
+# Seed de la DB
 npx prisma db seed
-echo "Starting server..."
+
+# Lancer le serveur
+echo "Starting NestJS server on port ${PORT:-4000}"
 node dist/main.js
